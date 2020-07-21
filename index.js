88649a463e3f08c54995c6247bbc2fd3fe8b2e75
Tzahi Levi <thetzahilevi@gmail.com>
2020-07-21 14:02:04 +0300

    add health and test request

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ const port = 8888;
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/health', (req,res) => {
+    res.send('Health ok');
+});
+
 app.post('/component', async (req,res) => {
     const zipStream = await mergeConfig(req.body);
     zipStream.pipe(res);
+});
+
+app.post('/test', (req, res) => {
+    setTimeout(() => res.json({ok: true}) , 5000);
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
